dd00b91f71bbe581352f7b34378027f79cec55b6
NickShermeister <ncsherman159@gmail.com>
2018-01-30 20:58:22 -0500

    pulling

---
+++ datastructures/day00/src/testArrayList.java
@@ -0,0 +1,41 @@
+public class testArrayList {
+    private Object[] A;
+    private int size;
+    private int type;
+
+
+    public testArrayList (int[] args){
+        A = args;
+        type = 0;
+    }
+
+    public testArrayList (double[] args){
+        A = args;
+        type = 1;
+    }
+
+    public testArrayList (String[] args){
+        A = args;
+        type = 2;
+    }
+
+    public void add(){
+        return;
+    }
+
+    private void grow() {
+        return;
+    }
+
+    public void remove(){
+        return;
+    }
+
+    public void get(int loc){
+
+    }
+
+    public void set(int loc, ){
+
+    }
+}
