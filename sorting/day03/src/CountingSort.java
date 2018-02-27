7fbc7273dd9520a17449085cd57dfa2239f73724
NickShermeister <ncsherman159@gmail.com>
2018-02-26 23:02:47 -0500

    sorting day 3

--- sorting/day03/src/CountingSort.java
+++ sorting/day03/src/CountingSort.java
@@ -2,12 +2,32 @@ public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: O(n + k)
+     * k is the largest number in the array
      *
      * k: maximum element in array A
      */
     static void countingSort(int[] A) {
-        // TODO
+        int max = -1;
+        for (int x : A){
+            if (max < x){
+                max = x;
+            }
+        }
+        int[] tracker = new int[max+1];
+
+        for(int y : A){
+            tracker[y]++;
+        }
+        int loc = 0;
+        for(int z = 0; z < tracker.length; z++){
+            while(tracker[z] > 0){
+                A[loc] = z;
+                tracker[z]--;
+                loc++;
+            }
+        }
+        return ;
     }
 
 }
--- sorting/day03/src/Problems.java
+++ sorting/day03/src/Problems.java
@@ -1,7 +1,17 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
         // TODO
+        for(int i = 0; i < A.length; i++){
+            A[i] = A[i] + 100;
+        }
+
+        CountingSort.countingSort(A);
+        for(int i = 0; i < A.length; i++){
+            A[i] = A[i] - 100;
+        }
     }
 
     /**
@@ -20,6 +30,26 @@ public class Problems {
      */
     static void countingSortByCharacter(String[] A, int n) {
         // TODO
+
+        LinkedList<String>[] L = new LinkedList[26];
+        for (int i = 0; i < 26; i++)
+            L[i] = new LinkedList<>();
+        int diff = -97;
+        for(String y : A){
+                int place = L[(int) (y.charAt(y.length()-1-n))+diff].size();
+
+                L[(int) (y.charAt(y.length()-1-n))+diff].add(place, y);
+
+        }
+
+        int j = 0; // index in A to place numbers
+        for (LinkedList<String> list : L) {
+            // TODO: Put all numbers in the linked lists into A
+            while(list.size()>0){
+                A[j] = list.pop();
+                j++;
+            }
+        }
     }
 
     /**
@@ -27,6 +57,9 @@ public class Problems {
      */
     static void sortStrings(String[] S, int stringLength) {
         // TODO
+        for(int i = 0; i < stringLength; i ++){
+            countingSortByCharacter(S, i);
+        }
     }
 
     /**
